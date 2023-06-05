523d20a9a959173000436a0861f1c3b04454ecac
Carlos Moraes <carloshrmoraes@gmail.com>
2023-06-05 13:49:32 -0300

    arrumando auth

--- src/componentes/AuthLoader.js
+++ src/componentes/AuthLoader.js
@@ -3,18 +3,15 @@ import { controleBD } from "../controleSupabase";
 export default async function AuthLoader() {
 
     const { data: sessionData, error: sessionError } = await controleBD.auth.getSession();
-    if (sessionError)
-        return;
+    if (!sessionData.session)
+        return { session: null, user: null };
     else {
-        if (!sessionData.session.user) {
-            return;
-        } else {
-            const { data: resultado, status } = await controleBD.from("funcionario").select("eGerente").eq("id", sessionData.session.user.id);
+        console.log(sessionData);
+        const { data: resultado, status } = await controleBD.from("funcionario").select("eGerente").eq("id", sessionData.session.user.id);
 
-            return {
-                user: { ...sessionData.session.user, eGerente: (status === 200 && resultado[0].eGerente) },
-                session: { ...sessionData.session }
-            };
+        return {
+            user: { ...sessionData.session.user, eGerente: (status === 200 && resultado[0].eGerente) },
+            session: { ...sessionData.session }
         };
     }
 }
--- src/componentes/HeaderGeral.jsx
+++ src/componentes/HeaderGeral.jsx
@@ -6,14 +6,19 @@ function HeaderGeral() {
     const navigate = useNavigate();
 
     return (
-        <header className='container-xl bg-body-secondary p-2 d-flex align-items-center justify-content-between'>
-            <button className='btn btn-success mx-2' onClick={() => navigate('/',)}>Home</button>
-            <div>
-                <p className='my-2 mx-4'>Usuario logado: {user.email}</p>
-                <button className='btn btn-info mx-2' onClick={() => navigate('/editfunc', { state: { user: { ...user, propria: true } } })}>Minha conta</button>
-                <button className='btn btn-warning mx-2' onClick={async () => await controleBD.auth.signOut()}>Sair</button>
-            </div>
-        </header>
+        <>{user && session ?
+            <header className='container-xl bg-body-secondary p-2 d-flex align-items-center justify-content-between'>
+                <button className='btn btn-success mx-2' onClick={() => navigate('/',)}>Home</button>
+                <div>
+                    <p className='my-2 mx-4'>Usuario logado: {user.email}</p>
+                    <button className='btn btn-info mx-2' onClick={() => navigate('/editfunc', { state: { user: { ...user, propria: true } } })}>Minha conta</button>
+                    <button className='btn btn-warning mx-2' onClick={async () => await controleBD.auth.signOut()}>Sair</button>
+                </div>
+            </header>
+            :
+            ""
+        }
+        </>
     );
 }
 
