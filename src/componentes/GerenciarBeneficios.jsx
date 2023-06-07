bd9f79cea1e91a4ac94ae70314d9de81737f2e7f
Carlos Moraes <carloshrmoraes@gmail.com>
2023-06-06 21:32:00 -0300

    alterar beneficio

--- src/componentes/GerenciarBeneficios.jsx
+++ src/componentes/GerenciarBeneficios.jsx
@@ -5,12 +5,12 @@ import ViewBeneficio from "./ViewBeneficio";
 function GerenciarBeneficios({ usuario }) {
 
     const [listaBeneficios, setListaBeneficios] = useState(null);
-    const [benAtual, setBenAtual] = useState(null)
+    const [benAtual, setBenAtual] = useState(null);
 
     useEffect(() => {
-        controleBD.from("cltbeneficios").select("*").then(({data: lista}) => {
+        controleBD.from("cltbeneficios").select("*").then(({ data: lista }) => {
             setListaBeneficios(lista);
-            
+
             controleBD.from("possui").select("*").eq("fk_funcionario_numcarteirat", usuario.numcarteirat).then(({ data, error }) => {
                 if (data && data[0])
                     setBenAtual(lista.find(b => b.categoria === data[0].fk_cltbeneficios_categoria));
@@ -19,18 +19,43 @@ function GerenciarBeneficios({ usuario }) {
     }, []);
 
     async function AlterarBeneficio() {
-
+        const sel = document.getElementById("selben").value;
+        setBenAtual(listaBeneficios.find(b => b.categoria === sel));
+        if (benAtual) {
+            const { error } = await controleBD.from("possui").update({
+                fk_cltbeneficios_categoria: sel,
+            }).eq("fk_funcionario_numcarteirat", usuario.numcarteirat);
+            if (error)
+                console.log(error);
+        } else {
+            const { error } = await controleBD.from("possui").insert({
+                fk_cltbeneficios_categoria: sel,
+                fk_funcionario_numcarteirat: usuario.numcarteirat
+            });
+            if (error)
+                console.log(error);
+        }
     }
 
     return (
-        <>
+        <div className="container-xl p-4 bg-info-subtle rounded">
+            <h4>Benefício</h4>
             {benAtual ?
-                <div className="container-xl px-4 py-2 my-4 bg-info-subtle rounded">
-                    <h4>Beneficio</h4>
-                    <ViewBeneficio beneficio={benAtual} />
-
-                </div> : ""}
-        </>
+                <ViewBeneficio beneficio={benAtual} />
+                : "Nenhum benefício definido."}
+            {usuario.eGerente && listaBeneficios ?
+                <div className="d-flex align-items-center">
+                    <h4>Definir beneficio:</h4>
+                    <select name="selben" id="selben" className="form-select">
+                        {listaBeneficios.map((b, i) =>
+                            <option key={i} value={b.categoria}>
+                                {b.categoria}
+                            </option>)}
+                    </select>
+                    <button className="btn btn-success" onClick={() => AlterarBeneficio()}>Confirma</button>
+                </div>
+                : ""}
+        </div>
     );
 }
 
