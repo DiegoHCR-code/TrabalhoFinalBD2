bef69adeeddd3587b90a6eb98cf96b5d4864a0a8
Vanessa Machado Silva <lampetrolaboratorioufu@gmail.com>
2023-06-07 09:54:52 -0300

    Arrumando pdf

--- src/componentes/Folhapagamento.jsx
+++ src/componentes/Folhapagamento.jsx
@@ -53,36 +53,45 @@ function FolhaPagamento() {
 
     return (
         <>{
+            
             <div className="w-75 m-auto">
+            <div className="d">
                 <h2>Gerar folha pagamento:</h2>
                 <div className="container-sm m-2 p-2 bg-info-subtle rounded">
                     <PainelFolha fkFuncionario={state.user} />
                 </div>
+                <button className="btn btn-primary btn-lg b button-danger" onClick={() => navigate(-1)}>Voltar</button>
+
+
+                <button className="btn btn-primary btn-lg b" onClick={generatePDF}> <i className="fas fa plus" />Gerar pdf</button>
                 <div ref={conponentPDF}>
-                <h2>Gerar folha pagamento:</h2>
-                <hr />
-                <table className="table table-light m-2">
-                    <thead>
-                        <tr>
-                            <th>Codigo</th>
-                            <th>Data</th>
-                            <th>Valor</th>
-                        </tr>
-                    </thead>
+                    <div className="pdf">
+                        <br />
 
-                    <thead>
-                        {folha.map(f => (
-                            <tr key={f.cod}><th>{f.cod}</th><th>{f.mes_ano}</th><th>R${f.total_mensal}.00</th></tr>
-                        ))}
+                        <h3>Folha pagamento:</h3>
+                        <hr />
+                        <br />
+                        <table className="table table-white m-2">
+                            <thead>
+                                <tr>
+                                    <th>Codigo</th>
+                                    <th>Data</th>
+                                    <th>Valor</th>
+                                </tr>
+                            </thead>
 
-                    </thead>
-                </table>
+                            <thead>
+                                {folha.map(f => (
+                                    <tr key={f.cod}><th>{f.cod}</th><th>{f.mes_ano}</th><th>R${f.total_mensal}.00</th></tr>
+                                ))}
+
+                            </thead>
+                        </table>
+                    </div>
                 </div>
-                <button className="btn btn-primary btn-lg" onClick={() => navigate(-1)}>Voltar</button>
 
 
-                <button className="btn btn-primary btn-lg"onClick={ generatePDF}> <i className="fas fa plus" />Gerar pdf</button>
-
+            </div>
             </div>}
         </>
     );
